3c73477202e5e7586536843112201e8986aaa470
arydz <arydz@sii.pl>
2020-02-08 23:19:14 +0100

    Detaching / clearing entities in transaction

--- src/main/java/com/arydz/jpa/hibernate/demo/repository/CourseRepository.java
+++ src/main/java/com/arydz/jpa/hibernate/demo/repository/CourseRepository.java
@@ -50,8 +50,22 @@ public class CourseRepository {
 	// also
 	// persist those changes. That's because all operations in transactions are atomic!
 	public void playWithEntityManager() {
+
 		Course course = new Course("Web Services in 100 steps");
 		entityManager.persist(course);
+		Course courseNext = new Course("Angular JS in 100 steps");
+		entityManager.persist(courseNext);
+		entityManager.flush(); // Changes that will be made until flush, will be send to database.
+		// It's important when we are using clear or detach method. If we don't flush data and next we will clear entity manager, then it won't
+		// know wat to track and send to database. No persistence operation will be invoked!
+
+		entityManager.clear(); // This detach all tracked objects in current transaction - it's instead of those two detach method invokes
+		//entityManager.detach(course); // This method cause that changes are no longer tracked for this object!
+		//entityManager.detach(courseNext); // This method cause that changes are no longer tracked for this object!
+
 		course.setName("Web Services in 100 steps - Updated");
+//		entityManager.flush();
+		courseNext.setName("Angular JS in 100 steps - Updated");
+//		entityManager.flush();
 	}
 }
